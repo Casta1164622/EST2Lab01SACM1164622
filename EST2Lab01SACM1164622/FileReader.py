f912c8649cafa5517cbf9ab1bbd719207dbe9e1b
Casta1164622 <sacastanedam@correo.url.edu.gt>
2023-09-04 19:40:07 -0600

    Implementada funcionalidad del arbol B

--- EST2Lab01SACM1164622/FileReader.py
+++ EST2Lab01SACM1164622/FileReader.py
@@ -3,10 +3,8 @@ import json
 def readCSVFile():
     data = [] 
     myFile = open(r'C:\Users\CASTA\Downloads\input.csv')
-    print("The content of CSV file is:")
     text = myFile.readline()
     while text != "":
-        print(text, end="")
         data.append(text)
         text = myFile.readline()
         
